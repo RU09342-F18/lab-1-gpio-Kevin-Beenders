eaf670243a9705e1dffd3c143a46a0bba0f80158
Kevin Beenders <35497869+Kevin-Beenders@users.noreply.github.com>
2018-09-17 13:23:19 -0400

    Create main.c

---
+++ Simple Blink/main.c
@@ -0,0 +1,22 @@
+#include <msp430.h> 
+
+
+/**
+ * main.c
+ */
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	
+	P1SEL &= ~BIT0;
+	P1DIR |= BIT0;
+
+	while (1)
+	{
+	    P1OUT ^= BIT0;
+	    __delay_cycles(1000000);
+	}
+
+
+	return 0;
+}
