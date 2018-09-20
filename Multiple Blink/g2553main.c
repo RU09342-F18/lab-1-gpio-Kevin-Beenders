e93afb8f2361470d5680ba61a91c3c53c38d80f4
Kevin Beenders <35497869+Kevin-Beenders@users.noreply.github.com>
2018-09-20 10:43:39 -0400

    Update g2553main.c

--- Multiple Blink/g2553main.c
+++ Multiple Blink/g2553main.c
@@ -1,45 +1,28 @@
 #include <msp430.h> 
 
 
-/**
- * main.c
- */
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	P1DIR |= BIT0 | BIT6;       //assigns bit0 and bit6 to be outputs
-	P1SEL &= ~(BIT0 + BIT6);     //assigns 0s to bit0 and bit6
-	int a;
-	int b;
-	a = 0;
-	b = 0;
-
-
-	while(1)
-	{
-
-	    if (a < 5)
-	    {
-	    P1OUT ^= BIT0;
-	    a += 1;
-	    }
-	    else
-	    {
-	        a = 0;
-	    }
-
-	    if (b < 3)
-	    {
-	    P1OUT ^= BIT6;
-	    b += 1;
-	    }
-	    else
-	    {
-	        b = 0;
-	    }
-	    __delay_cycles(1000000);
-	}
-
-	
-	return 0;
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    P1DIR |= BIT0 | BIT6; //assigns BIT0 and BIT6 to have an output direction
+    P1SEL &= ~(BIT0 + BIT6); //assigns value of 0 to Pin1 and Pin6 in P1SEL SFR
+    int a = 0;
+    int b = 0;
+    while(1)
+    {
+        a += 1;
+        b += 1;
+        __delay_cycles(50000);
+        if (a == 25)
+        {
+            P1OUT ^= BIT0;
+            a = 0;
+        }
+        if (b == 10)
+        {
+            P1OUT ^= BIT6;
+            b = 0;
+        }
+    }
+    return 0;
 }
